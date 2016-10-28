2e71d38d6a5613a33012ecc2bcfeb125402694ea
lrlimon <lrlimon@gmail.com>
2016-10-27 21:33:35 -0500

    fifth commit

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,6 +1,6 @@
 class Inventory < ActiveRecord::Base
   belongs_to :category
-  has_many :works
+  has_one :work
 
   validates :size, numericality: { greater_than: 0 }
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,16 @@
 class Work < ActiveRecord::Base
   belongs_to :user
   belongs_to :inventory
+
+  validate :validates_user
+
+  def validates_user
+    if self.user_id.nil?
+      errors.add(:user_id, 'is required')
+    else
+      if User.where(:id => self.user_id).size == 0
+        errors.add(:user_id, 'must exist')
+      end
+    end
+  end
 end
