20ad117fdbc8670f9741475ca1c0755fc2ade84b
lrlimon <lrlimon@gmail.com>
2016-10-28 01:35:30 -0500

    additional commit for category and inventory tables population

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-u1 = User.create(:name => "User 1", :email => "user1@x.com")
-u2 = User.create(:name => "User 2", :email => "user2@x.com")
+#u1 = User.create(:name => "User 1", :email => "user1@x.com")
+#u2 = User.create(:name => "User 2", :email => "user2@x.com")
 
-#i1 = Inventory.create(:name => "Inventory 1", :serial => "Serial A")
-#i2 = Inventory.create(:name => "Inventory 2", :serial => "Serial B")
-#i3 = Inventory.create(:name => "Inventory 3", :serial => "Serial C")
+#Work.create(:description => "First work", :user => u1)
+#Work.create(:description => "Second work", :user => u2)
+#Work.create(:description => "Third work", :user => u2)
 
-Work.create(:description => "First work", :user => u1)
-Work.create(:description => "Second work", :user => u2)
-Work.create(:description => "Third work", :user => u2)
+c1 = Category.create(:name => "Category 1")
+c2 = Category.create(:name => "Category 2")
+
+Inventory.create(:serial => "Serial A", :size => 10, :description => "Inventory 1", :category => c1)
+Inventory.create(:serial => "Serial B", :size => 12, :description => "Inventory 2", :category => c2)
+Inventory.create(:serial => "Serial C", :size => 8), :description => "Inventory 3", :category => c1)
