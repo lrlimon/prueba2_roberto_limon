ae604f34d0dcc8eb5f69bad6142e7d4d834822ef
lrlimon <lrlimon@gmail.com>
2016-10-27 22:25:40 -0500

    nineth commit

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,8 +1,14 @@
 class Inventory < ActiveRecord::Base
   belongs_to :category
-  has_one :work, :dependent => :delete_all
+  has_one :work, :dependent => :delete
+
+  after_destroy :create_log
 
   scope :size_equal, -> (s) { where(:size => s) }
 
   validates :size, numericality: { greater_than: 0 }
+
+  def create_log
+    Log.create("The item with serial #{self.serial} has been deleted")
+  end
 end
